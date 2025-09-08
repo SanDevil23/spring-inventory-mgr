ea0fa83b1359f3f8e56a33c9d43e05963c4fe1e6
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-09-09 01:34:58 +0530

    product service class implementation

---
+++ src/main/java/com/oms/inventory_service/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.oms.inventory_service.dao;
+
+import com.oms.inventory_service.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
--- src/main/java/com/oms/inventory_service/service/ProductService.java
+++ src/main/java/com/oms/inventory_service/service/ProductService.java
@@ -1,7 +1,48 @@
 package com.oms.inventory_service.service;
 
+import com.oms.inventory_service.dao.ProductRepository;
+import com.oms.inventory_service.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProductService implements IProductService{
+    private final ProductRepository productRepository;
+
+    @Autowired
+    ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public Product add(Product product) {
+        return null;
+    }
+
+    @Override
+    public Product fetchProductById(String productId) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        return List.of();
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        return List.of();
+    }
+
+    @Override
+    public void updateQuantity(String productId, int qty) {
+
+    }
+
+    @Override
+    public Product delete(String productId) {
+        return null;
+    }
 }
