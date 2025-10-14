82c8a22761a68a2c23185175f78922532a479706
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-10-15 00:48:07 +0530

    operating on the api functions

--- src/main/java/com/oms/inventory_service/controller/InventoryController.java
+++ src/main/java/com/oms/inventory_service/controller/InventoryController.java
@@ -5,11 +5,21 @@ import com.oms.inventory_service.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
 public class InventoryController {
+
+    // TODO: getProductById
+    // TODO: getProductByCategory
+    // TODO: getProductByName
+    // TODO: UpdateQuantity
+    // TODO: deleteProduct
+
     private final IProductService productService;
 
     public ResponseEntity<String> addProduct(@RequestBody ProductDto product){
@@ -20,4 +30,25 @@ public class InventoryController {
 
         return ResponseEntity.ok().body("New Product added successfully");
     }
+
+    public void getProductById(@RequestHeader long productId){
+        ProductDto product = productService.getProductById(productId);
+    }
+
+    public void getProductsByCategory(@RequestHeader String category){
+        List<ProductDto> product = productService.getProductsByCategory(category);
+    }
+
+    public void getProductsByName(@RequestHeader String name){
+        // to be defined
+    }
+
+    public void updateQty(@RequestHeader long id, @RequestHeader long qty){
+        // to be defined
+    }
+
+    public void delete(@RequestHeader long id){
+        // to be defined
+    }
+
 }
