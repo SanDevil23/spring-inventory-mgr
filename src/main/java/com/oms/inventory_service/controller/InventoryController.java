63e821e82aba4ee1fa8c6de3dafdcc49c928f7a6
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-10-16 23:20:45 +0530

    delete and update api finalized

--- src/main/java/com/oms/inventory_service/controller/InventoryController.java
+++ src/main/java/com/oms/inventory_service/controller/InventoryController.java
@@ -4,6 +4,7 @@ import com.oms.inventory_service.dto.ProductDto;
 import com.oms.inventory_service.models.Product;
 import com.oms.inventory_service.service.IProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -45,12 +46,18 @@ public class InventoryController {
         // to be defined
     }
 
-    public void updateQty(@RequestHeader long id, @RequestHeader long qty){
-        // to be defined
+    public ResponseEntity<String> updateQty(@RequestHeader long id, @RequestHeader long qty){
+        HttpStatus status = productService.updateQuantity(id, qty);
+        if (status == HttpStatus.INTERNAL_SERVER_ERROR)
+            return ResponseEntity.internalServerError().body("Failed to update quantity");
+
+        return ResponseEntity.ok().body("Product Quantity Updated Successfully");
     }
 
-    public void delete(@RequestHeader long id){
-        // to be defined
+    public ResponseEntity<String> delete(@RequestHeader long id){
+        ProductDto deletedProduct = productService.delete(id);
+        System.out.println(deletedProduct);     // to be logged to the client end
+        return ResponseEntity.ok("Product deleted from the inventory");
     }
 
 }
